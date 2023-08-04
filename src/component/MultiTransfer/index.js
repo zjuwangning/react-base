1f49e5622b20cbcdd8cb5a19115a72a12431f4e5
wangning <zjuwangning@zju.edu.cn>
2023-08-04 18:01:23 +0800

    feat: MultiTransfer style design

--- src/component/MultiTransfer/index.js
+++ src/component/MultiTransfer/index.js
@@ -1,33 +1,55 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'antd';
+import { Row, Col, Table, Button } from 'antd';
+import { RightOutlined, LeftOutlined } from '@ant-design/icons';
 import PropTypes from 'prop-types';
 import isEqual from 'react-fast-compare';
 import './index.less';
 
 /**
- * 面板组件
+ * 复用穿梭框
  */
 class MultiTransfer extends Component {
 	static defaultProps = {
-		leftStyle: {            // 主框样式
-			width: 260,
-			height: 400
+		// 数据源
+		data: {
+			source: {
+				title: '源数据',
+				columns: [],
+				dataSource: []
+			},
+			target: [
+				{
+					title: '目的数据',
+					columns: [],
+					dataSource: []
+				}
+			]
 		},
-		rightStyle: {           // 从框样式
-			width: 250
+		// 主框样式
+		leftStyle: {
+			width: 300,
+			height: 545
+		},
+		// 从框样式
+		rightStyle: {
+			width: 300,
+			height: 150
 		},
 	};
 	static propTypes = {
+		data: PropTypes.object,
 		leftStyle: PropTypes.object,
 		rightStyle: PropTypes.object,
 	};
 
 	constructor(props) {
 		super(props);
-		this.state = {};
+
+		const { data } = this.props
+		this.state = {
+			data
+		};
 	}
-
-
 
 	static getDerivedStateFromProps(nextProps, prevState) {
 		if (
@@ -49,23 +71,87 @@ class MultiTransfer extends Component {
 	}
 
 
+
+
+
 	render() {
-		const { expand } = this.state;
+		const { data } = this.state;
 		const { leftStyle, rightStyle } = this.props;
 
 		const leftSt = Object.assign({}, leftStyle)
 		const rightSt = Object.assign({}, rightStyle)
 
+		let sourceTitle = '源数据', sourceColumns = []
+		if (data && data.source && data.source.title) {
+			sourceTitle = data.source.title
+		}
+		if (data && data.source && data.source.columns) {
+			sourceColumns = data.source.columns
+		}
+
+		let target = [], targetArea = [];
+		if (data && data.target) {
+			target = data.target
+		}
+		for (let k in target) {
+			let targetTitle = '源数据'
+			if (target && target[k] && target[k].title) {
+				targetTitle = target[k].title
+			}
+			targetArea.push((
+				<Row>
+					<Col style={{margin: '0 3px'}}>
+						<div style={{marginTop: '40px'}}>
+							<Button
+								type="primary"
+								icon={<RightOutlined />}
+								disabled={true}
+							/>
+						</div>
+						<div style={{marginTop: '3px'}}>
+							<Button
+								type="primary"
+								icon={<LeftOutlined />}
+								disabled={true}
+							/>
+						</div>
+					</Col>
+					<Col>
+						<div className={'reid-transfer-right-box'}>
+							<Row className={'reid-transfer-title'} type={'flex'} justify={'space-between'}>
+								<Col>已选{}项</Col>
+								<Col>{targetTitle}</Col>
+							</Row>
+							<div style={rightSt}>
+
+							</div>
+						</div>
+					</Col>
+				</Row>
+			))
+		}
+
+
 		return (
 			<Row className={'reid-transfer-wrapper'} type={'flex'}>
 				<Col>
-					<div style={leftSt} className={'reid-transfer-left-box'}>
+					<div className={'reid-transfer-left-box'}>
 						<Row className={'reid-transfer-title'} type={'flex'} justify={'space-between'}>
 							<Col>已选{}项</Col>
-							<Col>标题</Col>
+							<Col>{sourceTitle}</Col>
 						</Row>
+						<div style={leftSt}>
+							<Table
+								style={{}}
+								size={'small'}
+								columns={sourceColumns}
+								rowKey={(record) => record.id || record.name}
+								dataSource={[]}
+							/>
+						</div>
 					</div>
 				</Col>
+				<Col>{targetArea}</Col>
 			</Row>
 		);
 	}
--- src/pages/entry/MultiTransfer/index.js
+++ src/pages/entry/MultiTransfer/index.js
@@ -10,15 +10,44 @@ function Index() {
 		return () => {}
 	}, []);
 
+
+	const sourceData = [];
+	const columns = [
+		{title: '序号', dataIndex: 'index', width: '10%', render: t=>t+1},
+		{title: '名称', dataIndex: 'index', width: '10%'},
+		{title: '容量', dataIndex: 'index', width: '10%'},
+	]
+
 	const data = {
-		source: {title: '可用硬盘', }
+		source: {
+			title: '可用硬盘',
+			columns: columns,
+			dataSource: sourceData
+		},
+		target: [
+			{
+				title: '数据盘',
+				columns: columns,
+				dataSource: []
+			},
+			{
+				title: '缓存盘',
+				columns: columns,
+				dataSource: []
+			},
+			{
+				title: '热备盘',
+				columns: columns,
+				dataSource: []
+			},
+		]
 	}
 
 	return (
 		<div className={'full-page'}>
 			Ant Design 的 Transfer 穿梭框组件只支持两栏中移动，无法应对一对多及多对多的数据排列场景。
 			<div style={{height: '30px'}}/>
-			<MultiTransfer />
+			<MultiTransfer data={data}/>
 		</div>
 	);
 }
